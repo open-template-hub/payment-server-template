fd8df4d723de6d064b8f2042bf03f70c2d4a7dad
furknyavuz <furknyavuz@gmail.com>
2020-06-02 21:30:19 +0300

    PayPal pt 1

---
+++ app/services/payment/paypalPayment.ts
@@ -0,0 +1,56 @@
+import { PaymentMethod } from '../../models/paymentMethod';
+import { PaymentMethodEnum } from './paymentWrapper';
+import axios from 'axios';
+import paypal from '@paypal/checkout-server-sdk';
+
+export class PayPalPayment implements PaymentMethod {
+
+ init = async (dbConn, paymentConfig, product, quantity) => {
+
+  let paypalClient = new paypal.core.PayPalHttpClient(new paypal.core.SandboxEnvironment(
+   paymentConfig.payload.client_id, paymentConfig.payload.secret
+  ));
+
+  const request = new paypal.orders.OrdersCreateRequest();
+  request.prefer("return=representation");
+  request.requestBody(
+  {
+   intent: "CAPTURE",
+   purchase_units: [{
+    amount: {
+     currency_code: product.payload.paypal.currency,
+     value: (product.payload.paypal.amount * quantity).toString()
+    }
+   }]
+  });
+
+  let order;
+  try {
+   order = await paypalClient.execute(request);
+  } catch (e) {
+   throw e;
+  }
+
+  return {history: order.result, id: order.result.id};
+ }
+
+ build = async (paymentConfig, external_transaction) => {
+  return {method: PaymentMethodEnum.PayPal, payload: {id: external_transaction.id}};
+ }
+
+ getTransactionHistory = async (dbConn, paymentConfig, external_transaction_id) => {
+
+  const headers = {
+   'X-CC-Api-Key': paymentConfig.payload.secret,
+   'X-CC-Version': '2018-03-22'
+  };
+
+  const response = await axios.get<any>(`${paymentConfig.payload.charge_url}/${external_transaction_id}`, {headers: headers});
+
+  return response.data.data;
+ }
+
+ receiptStatusUpdate = async (dbConn, paymentConfig, external_transaction_id, updated_transaction_history) => {
+  return null;
+ }
+}
