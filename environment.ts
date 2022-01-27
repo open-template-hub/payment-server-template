2041a81221bf57f10d4c66505dbd2a522e8c6aa5
Furkan Yavuz <furknyavuz@gmail.com>
2022-01-28 00:22:50 +0300

    update common

--- environment.ts
+++ environment.ts
@@ -2,20 +2,20 @@ import { DbArgs, EnvArgs, MqArgs, TokenArgs } from '@open-template-hub/common';
 
 export class Environment {
   constructor(private _args: EnvArgs = {} as EnvArgs) {
-    var tokenArgs = {
+    const tokenArgs = {
       accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
       responseEncryptionSecret: process.env.RESPONSE_ENCRYPTION_SECRET,
     } as TokenArgs;
 
-    var dbArgs = {
+    const dbArgs = {
       mongoDbConnectionLimit: process.env.MONGODB_CONNECTION_LIMIT,
       mongoDbUri: process.env.MONGODB_URI,
 
-      postgreSqlConnectionLimit: process.env.POSTGRESQL_CONNECTION_LIMIT,
-      postgreSqlUri: process.env.DATABASE_URL,
+      postgresqlConnectionLimit: process.env.POSTGRESQL_CONNECTION_LIMIT,
+      postgresqlUri: process.env.DATABASE_URL,
     } as DbArgs;
 
-    var mqArgs = {
+    const mqArgs = {
       messageQueueConnectionUrl: process.env.CLOUDAMQP_URL,
       paymentServerMessageQueueChannel:
         process.env.PAYMENT_SERVER_QUEUE_CHANNEL,
