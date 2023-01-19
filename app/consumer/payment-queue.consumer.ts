160104045dad32bbf5fe566ffc24e661c1ec461c
Fatih Turker <fatihturker35@gmail.com>
2023-01-19 10:30:25 +0300

    Integrate DLQ

--- app/consumer/payment-queue.consumer.ts
+++ app/consumer/payment-queue.consumer.ts
@@ -1,64 +1,95 @@
-import { ContextArgs, PaymentActionType, QueueConsumer, } from '@open-template-hub/common';
+import {
+  ContextArgs,
+  MessageQueueChannelType,
+  PaymentActionType,
+  QueueConsumer,
+  QueueMessage,
+} from '@open-template-hub/common';
 
 export class PaymentQueueConsumer implements QueueConsumer {
-
   private channel: any;
   private ctxArgs: ContextArgs = {} as ContextArgs;
 
-  init = ( channel: string, ctxArgs: ContextArgs ) => {
+  init = (channel: string, ctxArgs: ContextArgs) => {
     this.channel = channel;
     this.ctxArgs = ctxArgs;
     return this;
   };
 
-  onMessage = async ( msg: any ) => {
-    if ( msg !== null ) {
+  onMessage = async (msg: any) => {
+    if (msg !== null) {
       const msgStr = msg.content.toString();
-      const msgObj = JSON.parse( msgStr );
+      const msgObj = JSON.parse(msgStr);
 
       const message: PaymentActionType = msgObj.message;
 
       // Decide requeue in the error handling
       let requeue = false;
 
-      if ( message.example ) {
+      if (message.example) {
         const exampleHook = async () => {
-          console.log( 'Payment server example' );
+          console.log('Payment server example');
         };
 
-        await this.operate( msg, msgObj, requeue, exampleHook );
+        await this.operate(msg, msgObj, requeue, exampleHook);
       } else {
-        console.log( 'Message will be rejected: ', msgObj );
-        this.channel.reject( msg, false );
+        console.log('Message will be rejected: ', msgObj);
+        this.channel.reject(msg, false);
       }
     }
   };
 
   private operate = async (
-      msg: any,
-      msgObj: any,
-      requeue: boolean,
-      hook: Function
+    msg: any,
+    msgObj: any,
+    requeue: boolean,
+    hook: Function
   ) => {
     try {
       console.log(
-          'Message Received with deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj
+        'Message Received with deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj
       );
       await hook();
-      await this.channel.ack( msg );
+      await this.channel.ack(msg);
       console.log(
-          'Message Processed with deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj
+        'Message Processed with deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj
       );
-    } catch ( e ) {
+    } catch (e) {
       console.log(
-          'Error with processing deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj,
-          e
+        'Error with processing deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj,
+        e
       );
 
-      this.channel.nack( msg, false, requeue );
+      this.channel.nack(msg, false, requeue);
+    }
+  };
+
+  private moveToDLQ = async (msg: any, requeue: boolean) => {
+    try {
+      const orchestrationChannelTag =
+        this.ctxArgs.envArgs.mqArgs?.orchestrationServerMessageQueueChannel;
+
+      const message = {
+        sender: MessageQueueChannelType.PAYMENT,
+        receiver: MessageQueueChannelType.DLQ,
+        message: {
+          owner: MessageQueueChannelType.PAYMENT,
+          msg,
+        },
+      } as QueueMessage;
+
+      await this.ctxArgs.message_queue_provider?.publish(
+        message,
+        orchestrationChannelTag as string
+      );
+
+      this.channel.reject(msg, false);
+    } catch (e) {
+      console.log('Error while moving message to DLQ: ', msg);
+      this.channel.nack(msg, false, requeue);
     }
   };
 }
